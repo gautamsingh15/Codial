fecb13692ae91b237a45bfb0c4458d4863821e5f
Gautam Singh <gautam4uto@gamil.com>
2020-03-17 20:15:09 +0530

    Displayed user on the home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,12 @@
+const Post=require('../models/post');
+
 module.exports.home=function(req,res){
-    console.log(req.cookies);
-    return res.render('home',{
-        title:"home",
-    });
+    //console.log(req.cookies);
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title:"Codial | Home",
+            posts:posts
+        });
+    })
+   
 }
